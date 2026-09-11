8e944a60792cefbb9fd6ff9769555fd54e3f3607
Ikamjot Hundal <ikam.hundal@gmail.com>
2026-09-11 12:17:26 -0400

    Decided to keep it simple

--- automate_recycle_bin.py
+++ automate_recycle_bin.py
@@ -3,28 +3,17 @@
 #Revision History: 
 #   v1.0 - Initial version: Added the functionality to empty the recycle bin and handle exceptions. (September 5, 2026)
 # Added the main function to call the empty_recycle_bin function when the script is executed directly. (September 7, 2026)
-import os
 import winshell
+
 
 # Function to empty the recycle bin
 def empty_recycle_bin():
-
-    for item in winshell.recycle_bin():
-
-        try:
-            original_path = item.original_filename()
-            if original_path.lower().endswith(('.mp4', '.mkv')):
-                bin_file_path = item.real_filename()
-
-                if (bin_file_path and os.path.isfile(bin_file_path)):
-                    os.remove(bin_file_path)
-                    print(f"Deleted: {bin_file_path}")
-                else:
-                    print(f"File not found in recycle bin: {bin_file_path}")
-        except FileNotFoundError:
-            print(f"File not found in recycle bin: {item.real_filename()}")
-        except Exception as e:
-            print(f"Error processing item: {e}")
+    try:
+        # Empty the recycle bin
+        winshell.recycle_bin().empty(confirm=False, show_progress=True, sound=True)
+        print("Recycle bin emptied successfully.")
+    except Exception as e:
+        print(f"An error occurred while emptying the recycle bin: {e} or it is already empty.")
 
 if __name__ == "__main__":
     empty_recycle_bin()
