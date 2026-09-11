03e35f7ba8a8a3faa5d52c0acbb4744fae63998a
Ikamjot Hundal <ikam.hundal@gmail.com>
2026-09-11 11:52:38 -0400

    Modified the code to delete the videos only

--- automate_recycle_bin.py
+++ automate_recycle_bin.py
@@ -3,17 +3,22 @@
 #Revision History: 
 #   v1.0 - Initial version: Added the functionality to empty the recycle bin and handle exceptions. (September 5, 2026)
 # Added the main function to call the empty_recycle_bin function when the script is executed directly. (September 7, 2026)
+import os
 import winshell
 
+for item in winshell.recycle_bin():
+    original_path = item.original_filename()
 
 # Function to empty the recycle bin
 def empty_recycle_bin():
-    try:
-        # Empty the recycle bin
-        winshell.recycle_bin().empty(confirm=False, show_progress=True, sound=True)
-        print("Recycle bin emptied successfully.")
-    except Exception as e:
-        print(f"An error occurred while emptying the recycle bin: {e} or it is already empty.")
+
+    if original_path.lower().endswith(('.mp4', '.mkv')):
+        try:
+            # Empty the recycle bin
+            item.ultraandestory()
+            print("Recycle bin emptied successfully.")
+        except Exception as e:
+            print(f"An error occurred while emptying the recycle bin: {e} or it is already empty.")
 
 if __name__ == "__main__":
     empty_recycle_bin()
